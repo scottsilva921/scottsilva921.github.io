916b48fa9ca96557805184b397cbfa82ce1a5a61
scottsilva921 <68472827+scottsilva921@users.noreply.github.com>
2020-07-25 20:10:06 -0300

    Update script.js

--- script.js
+++ script.js
@@ -21,3 +21,26 @@ function closeNav() {
 	document.getElementById("main").style.marginLeft = "0";
 	document.body.style.backgroundColor = "white";
 	}
+
+
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function Dropdown(e) {
+	
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('#b')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
