cdfb0721456520194058e833fd13b1b7f5bf9fcd
scottsilva921 <scottsilva921@gmail.com>
2020-08-21 12:00:13 -0300

    Atualizando script.js

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ function barsX(x) {
 			/* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
 function openNav() {
 	if (cont%2==0){
-		document.getElementById("mySidenav").style.width = "250px";
-		document.getElementById("main").style.marginLeft = "250px";
+		document.getElementById("mySidenav").style.width = "300px";
+		document.getElementById("main").style.marginLeft = "300px";
 		document.body.style.backgroundColor = "#A9A9A9";
 		document.getElementById("main").style.filter = "grayscale(90%)";
 	}
@@ -56,4 +56,63 @@ function imgHover(){
 function imgOut(){
 	var o = document.getElementById("play");
 	o.remove();
+}
+
+function filmeOver(f, c, s){
+	document.getElementById(f).style.transition="0.3s ease";
+	document.getElementById(f).style.opacity="50%";
+
+	document.getElementById(c).style.top="81%"
+	document.getElementById(c).style.transition="0.3s ease";
+	document.getElementById(c).style.visibility="visible";
+
+	document.getElementById(s).style.top="3%";
+	document.getElementById(s).style.transition="0.3s ease"
+	document.getElementById(s).style.visibility="visible";
+}
+
+function filmeOut(f, c, s){
+	document.getElementById(f).style.opacity="100%";
+
+	document.getElementById(c).style.top="50%";
+	document.getElementById(c).style.transition="0.1s";
+	document.getElementById(c).style.visibility="hidden";
+
+	document.getElementById(s).style.top="50%";
+	document.getElementById(s).style.transition="0.1s";
+	document.getElementById(s).style.visibility="hidden";
+}
+
+function linkOver(e){
+	e.innerHTML += "<img src='https://upload.wikimedia.org/wikipedia/commons/8/84/Double_arrow_red.png' style='height:15px; padding-left:3px'>";
+}
+
+function linkOut(e, text){
+	e.innerHTML = text;
+	
+}
+
+function tamNavBar(){
+
+	
+	if (window.pageYOffset>160){
+		document.getElementById("imglogo").style.height="50px";
+		document.getElementById("imglogo").style.transition="0.5s";
+	}
+
+	if (window.pageYOffset<1){
+		document.getElementById("imglogo").style.height="300px";
+		document.getElementById("imglogo").style.transition="0.5s";
+	}
+	
+}
+
+function load(){
+	window.scrollTo(0,0)
+	tamNavBar();
+}
+
+function alteraLen(id){
+	document.getElementById(id).style.height="50px";
+	document.getElementById(id).style.transition="0.5s";
 }
